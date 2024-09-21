78fdea99c1b81573a7e1e3b572529d76d1edd32a
Matt602 <matthew.frierson062@gmail.com>
2024-09-21 00:49:09 -0400

    Finished the handling /healthz path.

--- main.go
+++ main.go
@@ -10,8 +10,12 @@ func main() {
 	const filepathRoot = "."
 	const port = "8080"
 
+	fileServer := http.FileServer(http.Dir(filepathRoot))
+
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	mux.Handle("/assets", http.FileServer(http.Dir(filepathRoot + "/assets")))
+	mux.HandleFunc("/healthz", readinessHandler)
 
 	server := &http.Server {
 		Handler: mux,  
@@ -20,4 +24,13 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	// set the Content-type header
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	// Write the status code
+	w.WriteHeader(http.StatusOK)
+	// Write "OK" to the response body
+	w.Write([]byte("OK"))
 }
